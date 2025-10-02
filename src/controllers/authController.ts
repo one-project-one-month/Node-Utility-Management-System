403caed6fafc32e4235fc1fdd764cce2a18ace44
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-10-03 00:11:34 +0700

    hotfix: change cookie config

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -11,7 +11,7 @@ import { successResponse } from '../common/apiResponse';
 const REFRESH_TOKEN_COOKIE_CONFIG = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production', // HTTPS in production
-  sameSite: 'strict' as const,
+  sameSite: 'lax' as const,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
