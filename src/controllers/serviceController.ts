08c845db5f24c722b959734b2f5b70faa4135acb
Thureinhtet99 <thureinhtet653@gmail.com>
2025-10-03 23:10:11 +0700

    feat: enhance service controllers and clean up index file

--- src/controllers/serviceController.ts
+++ src/controllers/serviceController.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NextFunction, Request, Response } from 'express';
 import { successResponse } from '../common/apiResponse';
 import {
@@ -39,13 +38,15 @@ export const serviceHistoryController = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    const result = await cutomerServiceHistory(req.validatedParams, req.validatedQuery);
-    successResponse(res, "Get service history successfull.", result, 200)
+    const result = await cutomerServiceHistory(
+      req.validatedParams,
+      req.validatedQuery
+    );
+    successResponse(res, 'Get service history successfull.', result, 200);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
-
+};
 
 // update customer service
 export const updateServiceController = async (
@@ -95,64 +96,3 @@ export const getServiceById = async (
     next(error);
   }
 };
-=======
-import { NextFunction, Request, Response } from "express";
-import { successResponse } from "../common/apiResponse";
-import { createCustomerService, getAllCustomerService, getCustomerServiceById, updateCustomerService } from "../services/customerService";
-
-
-
-//create customer service from client
-export const createServiceController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const service = await createCustomerService(req.validatedBody)
-
-        successResponse(res,
-            'Create service from  successful',
-            {
-                service
-            },
-            201)
-    } catch (error) {
-        next(error)
-    }
-}
-
-// update customer service
-export const updateServiceController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const data = { id: req.validatedParams.id, ...req.validatedBody }
-
-        const service = await updateCustomerService(data)
-        successResponse(res, "Service updated successful.", { service }, 200)
-    } catch (error) {
-        next(error)
-    }
-
-
-}
-
-//get all customer services 
-export const getAllServiceController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const result = await getAllCustomerService(req.validatedQuery)
-        successResponse(res, "Get all customer serivice successfull.", result, 200)
-    } catch (error) {
-        next(error)
-    }
-
-}
-
-//get customer service by id
-
-export const getServiceById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-
-    try {
-        const service = await getCustomerServiceById(req.validatedParams.id)
-        successResponse(res, "Get all customer serivice successfull.", { service }, 200)
-    } catch (error) {
-        next(error)
-    }
-
-}
->>>>>>> 7ad34e3 (pulled from dev)
