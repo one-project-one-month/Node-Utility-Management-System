4ed57581f2a7a59b85a152b6a23d0d57d710a252
HtunAungKyaw73 <htunaungkyaw730@gmail.com>
2025-10-03 14:00:50 +0700

    v1

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -10,15 +10,9 @@ import {
   NotFoundError,
   UnauthorizedError,
 } from '../common/errors';
-<<<<<<< HEAD
-import { LoginType } from '../validations/authSchema';
-
-export async function loginService(data: LoginType) {
-=======
 import { LogInType } from '../validations/authSchema';
 
 export async function loginService(data: LogInType) {
->>>>>>> dev
   // Find user by email
   const user = await prisma.user.findUnique({
     where: { email: data.email },
