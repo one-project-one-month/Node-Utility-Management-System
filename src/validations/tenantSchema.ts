bca311af3b7382faea38f8281945b89a91f842ba
Yamin <yaminnyein227580@gmail.com>
2025-10-05 13:35:35 +0630

    fix:update and create tenant

--- src/validations/tenantSchema.ts
+++ src/validations/tenantSchema.ts
@@ -2,12 +2,23 @@ import { z } from 'zod';
 
 export const CreateTenantSchema = z
   .object({
-    names: z.array(z.string().min(1, 'Name is required')),
-    emails: z.array(z.email('Invalid email')),
-    nrcs: z.array(z.string().min(3, 'NRC is required')),
-    phone_nos: z.array(z.string().min(6, 'Phone number is too short')),
-    emergency_nos: z.array(z.string().min(6, 'Emergency number is too short')),
-    room_id: z.uuid({ version: 'v4' }),
+    names: z
+      .array(z.string().min(1, 'Name is required'))
+      .nonempty('Names cannot be empty'),
+    emails: z
+      .array(z.email('Invalid email'))
+      .nonempty('Emails cannot be empty'),
+
+    nrcs: z
+      .array(z.string().min(3, 'NRC is required'))
+      .nonempty('NRCs cannot be empty'),
+    phone_nos: z
+      .array(z.string().min(6, 'Phone number is too short'))
+      .nonempty('Phone numbers cannot be empty'),
+    emergency_nos: z
+      .array(z.string().min(6, 'Emergency number is too short'))
+      .nonempty('Emergency numbers cannot be empty'),
+    room_id: z.uuid({ message: 'Invalid room ID' }),
   })
   .refine(
     (data) => {
@@ -18,58 +29,65 @@ export const CreateTenantSchema = z
       message:
         'Some array fields (names, emails and nrcs) must have the same length',
     }
-  );
-
-export const UpdateTenantSchema = z
-  .object({
-    names: z.array(z.string().min(1, 'Name is required')).optional(),
-    emails: z.array(z.email('Invalid email')).optional(),
-    nrcs: z.array(z.string().min(3, 'NRC is required')).optional(),
-    phone_nos: z
-      .array(z.string().min(6, 'Phone number is too short'))
-      .optional(),
-    emergency_nos: z
-      .array(z.string().min(6, 'Emergency number is too short'))
-      .optional(),
-    room_id: z.uuid({ version: 'v4' }).optional(),
-  })
-  //at least one field must be provided
-  .refine(
-    (data) =>
-      data.names ||
-      data.emails ||
-      data.nrcs ||
-      data.phone_nos ||
-      data.emergency_nos ||
-      data.room_id,
-      // Object.keys(data).length > 0, // cmt: Object.keys(data).length > 0 will be more efficient
-    {
-      message: 'At least one field must be provided for update',
-    }
   )
-  .refine(
-    (data) => {
-      // if none of the array fields are provided, it's valid
-      if (!data.names && !data.emails && !data.nrcs) {
-        return true;
+  .strict();
+
+export const UpdateTenantSchema = (expectedLengths: {
+  names: number;
+  emails: number;
+  nrcs: number;
+}) =>
+  z
+    .object({
+      names: z
+        .array(z.string().min(1, 'Name is required'))
+        .nonempty('Names cannot be empty')
+        .optional(),
+      emails: z
+        .array(z.email('Invalid email'))
+        .nonempty('Emails cannot be empty')
+        .optional(),
+      nrcs: z
+        .array(z.string().min(3, 'NRC is required'))
+        .nonempty('NRCs cannot be empty')
+        .optional(),
+      phone_nos: z
+        .array(z.string().min(6, 'Phone number is too short'))
+        .nonempty('Phone numbers cannot be empty')
+        .optional(),
+      emergency_nos: z
+        .array(z.string().min(6, 'Emergency number is too short'))
+        .nonempty('Emergency numbers cannot be empty')
+        .optional(),
+      room_id: z.uuid({ message: 'Invalid room ID' }).optional(),
+    })
+    .refine(
+      (data) =>
+        data.names ||
+        data.emails ||
+        data.nrcs ||
+        data.phone_nos ||
+        data.emergency_nos ||
+        data.room_id,
+      {
+        message: 'At least one field must be provided for update',
       }
-
-      // pick the first non-undefined array to use as reference length
-      const referenceArray = data.names || data.emails || data.nrcs;
-      const refLength = referenceArray?.length ?? 0;
-
-      // check each provided array against the reference length
-      return (
-        (!data.names || data.names.length === refLength) &&
-        (!data.emails || data.emails.length === refLength) &&
-        (!data.nrcs || data.nrcs.length === refLength)
-      );
-    },
-    {
-      message:
-        'Some array fields (names, emails and nrcs) must have the same length',
-    }
-  );
+    )
+    .refine(
+      (data) => {
+        if (data.names && data.names.length !== expectedLengths.names)
+          return false;
+        if (data.emails && data.emails.length !== expectedLengths.emails)
+          return false;
+        if (data.nrcs && data.nrcs.length !== expectedLengths.nrcs)
+          return false;
+        return true;
+      },
+      {
+        message: 'Array length must match the existing data length.',
+      }
+    )
+    .strict();
 
 export const GetTenantParamSchema = z.object({
   tenantId: z.uuid({ version: 'v4' }),
@@ -96,6 +114,6 @@ export const GetAllTenantsQuerySchema = z
   .strict();
 
 export type CreateTenantType = z.infer<typeof CreateTenantSchema>;
-export type UpdateTenantType = z.infer<typeof UpdateTenantSchema>;
+export type UpdateTenantType = z.infer<ReturnType<typeof UpdateTenantSchema>>;
 export type GetUserParamType = z.infer<typeof GetTenantParamSchema>;
 export type GetAllTenantsQueryType = z.infer<typeof GetAllTenantsQuerySchema>;
