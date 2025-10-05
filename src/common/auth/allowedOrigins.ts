741e3b9c511ebc73aa9fea8544baaf797ea3e863
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-10-05 17:19:55 +0700

    refactor: add dev deployed api url to allowedOrigins

--- src/common/auth/allowedOrigins.ts
+++ src/common/auth/allowedOrigins.ts
@@ -7,4 +7,5 @@ export const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:5175',
   'https://node-utility-management-system.onrender.com',
+  'https://node-utility-management-system-production.up.railway.app',
 ];
