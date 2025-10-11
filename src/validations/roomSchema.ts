d83c9f357a3a5860655e7a276e405e41e250ad9e
Chit Hmue Than Thar <juliawilson757@gmail.com>
2025-10-11 21:57:02 +0630

    Update: fix the roomschema pagination

--- src/validations/roomSchema.ts
+++ src/validations/roomSchema.ts
@@ -1,5 +1,6 @@
 import z from 'zod';
 import { RoomStatus } from '../../generated/prisma';
+import { PaginationQuerySchema } from './paginationSchema';
 
 export const CreateRoomSchema = z.object({
     room_no: z.number().int().min(1, 'Room number is required and choose a positive number in the list'),
@@ -39,7 +40,7 @@ export const RoomIdSchema = z.object({
 });
 
 //Add Query Schema (for filtering and pagination)
-export const GetAllRoomSchema = z.object({
+export const GetAllRoomSchema = PaginationQuerySchema.extend({
   room_no: z.string().optional(),
   floor: z.number().int().optional(),
   status: z
@@ -47,8 +48,13 @@ export const GetAllRoomSchema = z.object({
     RoomStatus, "Room Status must be one of 'Available', 'Rented', 'Purchased', 'InMaintenance"
     )
     .optional(),
-  page: z.string().optional(),
-  limit: z.string().optional(),
+  is_active: z
+    .string()
+    .refine((val) => val === 'true' || val === 'false', {
+      message: 'is_active must be either "true" or "false"',
+    })
+    .transform((val) => val === 'true')
+    .optional(),
 });
 
 
