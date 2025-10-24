1b871f36a5eb9abbb7348f5fc59f517a3cb6950b
HtunAungKyaw73 <htunaungkyaw730@gmail.com>
2025-10-24 23:59:33 +0700

    restore:mail-service

--- src/common/utils/mail-service/mailTransporter.ts
+++ src/common/utils/mail-service/mailTransporter.ts
@@ -24,23 +24,18 @@ export async function mailOptionConfig(
 
 export async function mailTransporter(): Promise<nodemailer.Transporter> {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587, // Change from 465 to 587
-    secure: false, // Must be false for port 587
+    service: "gmail",
     auth: {
-      type: "OAuth2",
       user: process.env.EMAIL_USER,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   try {
     await transporter.verify();
-    console.log('mail server connection successful');
+    console.log('SMTP server connection successful');
   } catch (error) {
-    console.error('mail verification failed:', error);
+    console.error('SMTP verification failed:', error);
     throw error;
   }
   
