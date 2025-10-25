359e45f2afc06052a6f4e4aefdf13dedb5eb5447
HtunAungKyaw73 <htunaungkyaw730@gmail.com>
2025-10-25 12:17:28 +0700

    fix:eslint

--- src/common/utils/mail-service/resendMailTransporter.ts
+++ src/common/utils/mail-service/resendMailTransporter.ts
@@ -1,4 +1,4 @@
-import {Resend} from 'resend';
+import { Resend } from 'resend';
 
 export interface MailData {
   name: string;
@@ -7,20 +7,18 @@ export interface MailData {
   htmlContent?: string;
   subject: string;
 }
-export interface MailOptions{
-    from: string;
-    to: string;
-    subject: string;
-    text: string;
-    html?: string;
+export interface MailOptions {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+  html?: string;
 }
 
-export function mailOptionConfig(
-  mailData: MailData
-):MailOptions{
+export function mailOptionConfig(mailData: MailData): MailOptions {
   const { name, to, message, subject, htmlContent } = mailData;
   const mailOptions = {
-    from: "nestflowums@resend.dev",
+    from: 'nestflowums@resend.dev',
     to: to,
     subject: subject,
     text: `Dear ${name},\n\n${message}\n\nBest regards,\nNest Flow`,
@@ -31,17 +29,13 @@ export function mailOptionConfig(
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function mailSend(mailConfig:MailOptions)
-{
-    try{
-        const info = await resend.emails.send(mailConfig);
-        console.log("Mail successfully sent: ", info.data?.id);
-        return info.data;
-    }
-    catch(error)
-    {
-        console.error("Mail sent failed: ", error);
-        throw error;
-    }
+export async function mailSend(mailConfig: MailOptions) {
+  try {
+    const info = await resend.emails.send(mailConfig);
+    console.log('Mail successfully sent: ', info.data?.id);
+    return info.data;
+  } catch (error) {
+    console.error('Mail sent failed: ', error);
+    throw error;
+  }
 }
-
