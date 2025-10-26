f26357237e35d2711ffb7faae9ad7a84d00404d8
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-10-26 12:45:21 +0700

    fix: invoice service res body

---
+++ src/common/utils/obj-flattener/invoiceConfig.ts
@@ -0,0 +1,13 @@
+export const INVOICES_FLATTENER_CONFIG = {
+  id: 'id',
+  invoiceNo: 'invoiceNo',
+  status: 'status',
+  receiptSent: 'receiptSent',
+  billId: 'billId',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+  tenantName: 'bill.room.tenant.name',
+  roomNo: 'bill.room.roomNo',
+  totalAmount: 'bill.totalAmount',
+  billDueDate: 'bill.dueDate',
+};
