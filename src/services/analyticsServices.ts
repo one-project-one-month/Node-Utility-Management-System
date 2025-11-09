d0d5607b9247e038c5d8d7ef9463fcf631d4bfd0
Thureinhtet99 <thureinhtet653@gmail.com>
2025-11-09 14:10:18 +0700

    refactor: refactoring analytics for contract types

--- src/services/analyticsServices.ts
+++ src/services/analyticsServices.ts
@@ -3,33 +3,48 @@ import prisma from '../lib/prismaClient';
 
 // Contract analytics
 export const contractTypeAnalyticsService = async () => {
-  // Get tenant count grouped by contract type
-  const contracts = await prisma.contract.groupBy({
-    by: ['contractTypeId'],
-    _count: {
-      tenantId: true,
+  // // Get tenant count grouped by contract type
+  // const contracts = await prisma.contract.groupBy({
+  //   by: ['contractTypeId'],
+  //   _count: {
+  //     tenantId: true,
+  //   },
+  // });
+
+  // if (!contracts.length) {
+  //   throw new NotFoundError('No contracts found');
+  // }
+
+  // const result = await Promise.all(
+  //   contracts.map(async (stat) => {
+  //     const contractType = await prisma.contractType.findUnique({
+  //       where: { id: stat.contractTypeId },
+  //       select: {
+  //         name: true,
+  //       },
+  //     });
+
+  //     return {
+  //       contractType,
+  //       tenantCount: stat._count.tenantId,
+  //     };
+  //   })
+  // );
+
+  // return result;
+
+  const contractTypes = await prisma.contractType.findMany({
+    select: {
+      name: true,
+      _count: {
+        select: {
+          contract: true,
+        },
+      },
     },
   });
 
-  if (!contracts.length) {
-    throw new NotFoundError('No contracts found');
-  }
+  if (!contractTypes.length) throw new NotFoundError('No contract types found');
 
-  const result = await Promise.all(
-    contracts.map(async (stat) => {
-      const contractType = await prisma.contractType.findUnique({
-        where: { id: stat.contractTypeId },
-        select: {
-          name: true,
-        },
-      });
-
-      return {
-        contractType,
-        tenantCount: stat._count.tenantId,
-      };
-    })
-  );
-
-  return result;
+  return contractTypes;
 };
